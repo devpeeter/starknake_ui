cde94bcd29e2dc6c86a904d1c3e38819920b8428
Peter Fatukasi <gregieprice@gmail.com>
2025-10-04 00:50:54 +0100

    Delete .next directory

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app/page.tsx -> @/components/Game\":{\"id\":\"app/page.tsx -> @/components/Game\",\"files\":[\"static/chunks/_app-pages-browser_components_Game_tsx.js\"]}}"
